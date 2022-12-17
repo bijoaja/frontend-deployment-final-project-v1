46e092ede4669a4cdeb49b7ff5c21e5ad09a489d
bijoaja <joelbinsar@gmail.com>
2022-12-18 02:01:13 +0700

    deployment-final-project-v1

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Joel Binsar Jupiter" // TODO: replace this
+const studentId = "FE2677616" // TODO: replace this
+
+export { url, studentName, studentId }
