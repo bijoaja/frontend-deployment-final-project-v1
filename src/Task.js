affc11899580583e6f6a3f6c5207b227c849f06b
bijoaja <joelbinsar@gmail.com>
2022-12-19 19:14:31 +0700

    second change

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://galery-app-43036.web.app/" // TODO: replace this
 const studentName = "Joel Binsar Jupiter" // TODO: replace this
 const studentId = "FE2677616" // TODO: replace this
 
